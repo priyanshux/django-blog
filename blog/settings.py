6fc6a1babd512cd544a75d60912625a58960d13c
Priyanshu <psingh1002@gmail.com>
2019-12-28 01:30:42 +0530

    revert

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pqpj*wot_0wa09uh4=5c_m8eg#w=^xsiofw%xn2i%ocpa)_!nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.amazonaws.com']
 

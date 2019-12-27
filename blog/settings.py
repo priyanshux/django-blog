1dca914c6f2a041396a7b10a24dd0635fc220ddd
Priyanshu <psingh1002@gmail.com>
2019-12-28 01:13:41 +0530

    debug false

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pqpj*wot_0wa09uh4=5c_m8eg#w=^xsiofw%xn2i%ocpa)_!nh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.amazonaws.com']
 
